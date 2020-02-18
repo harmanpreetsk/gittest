4ad7b03f8cf8bdbe9be4461a2c50fd17e397b409
Harmanpreet Singh <54905900+harmanpreetsk@users.noreply.github.com>
2020-02-18 16:48:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set("view engine","handlebars");
 app.get("/",(req,res)=>{
     res.render("home");
 })
-const PORT =process.env.PORT || 3000
+const PORT =process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("Server started");
 })
